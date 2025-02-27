a977d0f26a33c14a560fe5cf2d493d74ba849adf
Esthermandy55 <85802143+Esthermandy55@users.noreply.github.com>
2025-02-27 20:17:49 +0100

    Moved image to public for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import Image from "./Image";
-import Schedule from "./assets/Schedule.png";
 
 function App() {
   return (
     <>
-      <Image src={Schedule} width="50%" height="50%" />
+      <Image src="/Schedule.png" width="50%" height="50%" />
     </>
   );
 }
