818a02d850e012d7c38af902d8292e8fd2b68963
Esthermandy55 <85802143+Esthermandy55@users.noreply.github.com>
2025-02-27 20:49:27 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import Image from "./Image";
 function App() {
   return (
     <>
-      <Image
-        src="/imagepropsassignment/Schedule.png"
-        width="50%"
-        height="50%"
-      />
+      <Image src="/Schedule.png" width="50%" height="50%" />
     </>
   );
 }
