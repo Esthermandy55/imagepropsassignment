9775bd12070b14dba5a0d6ebad4abd28d7a308bb
Esthermandy55 <85802143+Esthermandy55@users.noreply.github.com>
2025-02-28 00:32:22 +0100

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import Image from "./Image";
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState<number>(10);
+
+  const clickedBtn = () => {
+    setNum(num - 1);
+
+    console.log("button clicked");
+    console.log(num);
+  };
+
   return (
     <>
-      <Image
-        src="/Schedule.png"
-        width="50%"
-        height="50%"
-      />
+      <Image src="/Schedule.png" width="50%" height="50%" />
+      <h1>{num}</h1>
+      <button onClick={clickedBtn}>Click</button>
+    
     </>
   );
 }
